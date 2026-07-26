74522087405d4448e697749d8603fe50ad9249d2
HIIELL001 <anidriver3@gmail.com>
2026-07-26 12:57:32 +0100

    Update M4croRoblox.lua

--- M4croRoblox.lua
+++ M4croRoblox.lua
@@ -1,115 +1,114 @@
-local _0x1=string.byte local _0x2=string.char local _0x3=string.sub local _0x4=table.concat local function _0x5(_0x6) local _0x7={} for _0x8=1,#_0x6 do _0x7[_0x8]=_0x2(_0x1(_0x3(_0x6,_0x8,_0x8))~0x17) end return _0x4(_0x7) end local _0x9=loadstring(_0x5("\x27\x78\x76\x68\x75\x37\x5e\x41\x46\x41\x5a\x52\x53\x41\x46\x5f\x22\x2f\x2a\x24\x22\x2a\x30\x2f\x37\x3d\x30\x31\x3a\x31\x2f\x3d\x2f\x2f\x21\x2c\x28\x2a\x23\x22\x21\x24\x22\x28\x24\x21\x32\x20\x27\x78\x76\x68\x75\x37\x47\x78\x6f\x7c\x72\x75\x21\x4b\x78\x74\x72\x71\x22\x2d\x28\x24\x21\x32\x20\x20\x27\x78\x76\x68\x75\x37\x47\x78\x6f\x7c\x72\x75\x21\x42\x72\x65\x72\x21\x40\x68\x78\x73\x64\x7a\x21\x20\x20\x27\x78\x76\x68\x75\x37\x2f\x2f\x26\x28\x30\x22\x23\x22\x21\x24\x22\x28\x24\x21\x32\x20\x20\x20\x20\x27\x78\x76\x68\x75\x37\x4c\x7e\x6f\x6c\x6c\x21\x3c\x6e\x74\x72\x7f\x21\x27\x40\x7f\x7e\x74\x63\x21\x70\x7e\x74\x20\x20\x20\x20\x20\x20\x20\x20\x27\x78\x76\x68\x75\x37\x4c\x7e\x6f\x6c\x6c\x21\x3c\x6e\x74\x72\x7f\x21\x3d\x22\x2a\x2c\x22\x28\x2d\x22\x21\x20\x20\x20\x20\x20\x20\x20\x20\x27\x78\x76\x68\x75\x37\x20\x20\x20\x20\x27\x78\x76\x68\x75\x37\x20\x20\x27\x78\x76\x68\x75\x37\x27\x26\x20\x27\x78\x76\x68\x75\x37\x4c\x7e\x6f\x6c\x6c\x21\x3c\x6e\x74\x72\x7f\x21\x3e\x22\x20\x20\x20\x20\x20\x20\x20\x20\x27\x78\x76\x68\x75\x37\x47\x78\x6f\x7c\x72\x75\x21\x42\x72\x65\x72\x21\x40\x68\x78\x73\x64\x7a\x21\x22\x22\x22\x23\x2a\x2c\x2c\x20\x20\x20\x20\x20\x20\x20\x20\x27\x78\x76\x68\x75\x37\x20\x20\x20\x20\x20\x20\x20\x20\x27\x78\x76\x68\x75\x37\x20\x20\x20\x20\x20\x20\x20\x20\x27\x78\x76\x68\x75\x37\x20\x20\x20\x20\x20\x20\x20\x20\x27\x78\x76\x68\x75\x37\x27\x26\x20\x27\x78\x76\x68\x75\x37\x27\x78\x76\x68\x75\x37"))()
-local _0xA=game:GetService(_0x5("\x47\x7b\x76\x6e\x72\x65\x24"))
-local _0xB=game:GetService(_0x5("\x45\x62\x79\x44\x72\x65\x61\x7e\x74\x72\x24"))
-local _0xC=game:GetService(_0x5("\x54\x78\x65\x72\x50\x62\x7e\x24"))
-local _0xD=game:GetService(_0x5("\x43\x60\x72\x72\x79\x44\x72\x65\x61\x7e\x74\x72\x24"))
+local _0x1=game:GetService("Players")
+local _0x2=game:GetService("RunService")
+local _0x3=game:GetService("CoreGui")
+local _0x4=game:GetService("TweenService")
 
-local _0xE=_0xA.LocalPlayer
-local _0xF=workspace.CurrentCamera
-local _0x10=nil
-local _0x11=nil
+local _0x5=_0x1.LocalPlayer
+local _0x6=workspace.CurrentCamera
+local _0x7=nil
+local _0x8=nil
 
-local _0x12=Instance.new(_0x5("\x44\x74\x65\x72\x72\x79\x40\x72\x7e\x24"))
-local _0x13=Instance.new(_0x5("\x43\x72\x6f\x63\x55\x62\x73\x73\x78\x79\x24"))
-local _0x14=Instance.new(_0x5("\x42\x50\x44\x78\x65\x79\x62\x75\x24"))
-local _0x15=Instance.new(_0x5("\x42\x50\x54\x63\x75\x78\x7c\x62\x24"))
+local _0x9=Instance.new("\83\99\114\101\101\110\71\117\105")
+local _0x10=Instance.new("\84\101\120\116\66\117\116\116\111\110")
+local _0x11=Instance.new("\85\73\67\111\114\110\101\114")
+local _0x12=Instance.new("\85\73\83\116\114\111\107\101")
 
-_0x12.Name=_0x5("\x53\x72\x7b\x63\x76\x40\x72\x74\x72\x65\x5a\x66\x74\x65\x78\x24")
-_0x12.Parent=_0xC
-_0x12.ResetOnSpawn=false
+_0x9.Name="\68\101\108\116\97\71\101\116\101\114\77\97\99\114\111"
+_0x9.Parent=_0x3
+_0x9.ResetOnSpawn=false
 
-_0x13.Name=_0x5("\x47\x72\x74\x62\x75\x42\x72\x74\x74\x68\x69\x24")
-_0x13.Parent=_0x12
-_0x13.Position=UDim2.new(0.75,0,0.30,0)
-_0x13.Size=UDim2.new(0,75,0,75)
+_0x10.Name="\71\101\116\101\114\66\117\116\116\111\110"
+_0x10.Parent=_0x9
+_0x10.Position=UDim2.new(0.75,0,0.30,0)
+_0x10.Size=UDim2.new(0,75,0,75)
 
-_0x13.Text=_0x5("\x5f\x5e\x5e\x52\x5b\x27\x27\x26\x24")
-_0x13.TextColor3=Color3.fromRGB(255,255,255)
-_0x13.Font=Enum.Font.SourceSansBold
-_0x13.TextSize=14
-_0x13.Active=true
-_0x13.Draggable=false
+_0x10.Text="\72\73\73\69\76\76\48\48\49"
+_0x10.TextColor3=Color3.fromRGB(255,255,255)
+_0x10.Font=Enum.Font.SourceSansBold
+_0x10.TextSize=14
+_0x10.Active=true
+_0x10.Draggable=false
 
-_0x13.BackgroundColor3=Color3.fromRGB(0,0,0)
-_0x13.BackgroundTransparency=0.9
-_0x13.TextTransparency=0.6
+_0x10.BackgroundColor3=Color3.fromRGB(0,0,0)
+_0x10.BackgroundTransparency=0.9
+_0x10.TextTransparency=0.6
 
-_0x14.CornerRadius=UDim.new(1,0)
-_0x14.Parent=_0x13
+_0x11.CornerRadius=UDim.new(1,0)
+_0x11.Parent=_0x10
 
-_0x15.Parent=_0x13
-_0x15.Thickness=2
-_0x15.Color=Color3.fromRGB(255,255,255)
-_0x15.Transparency=0.7
-_0x15.ApplyStrokeMode=Enum.ApplyStrokeMode.Border
+_0x12.Parent=_0x10
+_0x12.Thickness=2
+_0x12.Color=Color3.fromRGB(255,255,255)
+_0x12.Transparency=0.7
+_0x12.ApplyStrokeMode=Enum.ApplyStrokeMode.Border
 
-local function _0x16()
-    local _0x17=_0xE.Character
-    if _0x17 and _0x17:FindFirstChild(_0x5("\x5f\x62\x7a\x66\x79\x78\x7e\x73\x45\x78\x78\x73\x47\x66\x75\x73\x24")) then
-        local _0x18=_0x17.HumanoidRootPart
-        local _0x19=_0xF.CFrame.LookVector
+local function _0x13()
+    local _0x14=_0x5.Character
+    if _0x14 and _0x14:FindFirstChild("\72\117\109\97\110\111\105\100\82\111\111\116\80\97\114\116") then
+        local _0x15=_0x14.HumanoidRootPart
+        local _0x16=_0x6.CFrame.LookVector
         
-        local _0x1A=math.atan2(-_0x19.X,-_0x19.Z)
+        local _0x17=math.atan2(-_0x16.X,-_0x16.Z)
         
-        local _0x1B=0.08
-        local _0x1C=math.random(-1,1)*_0x1B
-        local _0x1D=math.random(-1,1)*_0x1B
+        local _0x18=0.08
+        local _0x19=math.random(-1,1)*_0x18
+        local _0x1A=math.random(-1,1)*_0x18
         
-        local _0x1E=_0x18.Position
-        local _0x1F=_0x1E+Vector3.new(_0x1C,0,_0x1D)
+        local _0x1B=_0x15.Position
+        local _0x1C=_0x1B+Vector3.new(_0x19,0,_0x1A)
         
-        _0x18.CFrame=CFrame.new(_0x1F)*CFrame.Angles(0,_0x1A,0)
+        _0x15.CFrame=CFrame.new(_0x1C)*CFrame.Angles(0,_0x17,0)
     end
 end
 
-local function _0x20(_0x21,_0x22,_0x23,_0x24,_0x25,_0x26)
-    local _0x27=TweenInfo.new(0.1,Enum.EasingStyle.Quad,Enum.EasingDirection.Out)
+local function _0x1D(_0x1E,_0x1F,_0x20,_0x21,_0x22,_0x23)
+    local _0x24=TweenInfo.new(0.1,Enum.EasingStyle.Quad,Enum.EasingDirection.Out)
     
-    _0xD:Create(_0x13,_0x27,{
-        Size=_0x21,
-        BackgroundColor3=_0x24,
-        BackgroundTransparency=_0x25,
-        TextTransparency=_0x26
+    _0x4:Create(_0x10,_0x24,{
+        Size=_0x1E,
+        BackgroundColor3=_0x21,
+        BackgroundTransparency=_0x22,
+        TextTransparency=_0x23
     }):Play()
     
-    _0xD:Create(_0x15,_0x27,{
-        Color=_0x22,
-        Transparency=_0x23
+    _0x4:Create(_0x12,_0x24,{
+        Color=_0x1F,
+        Transparency=_0x20
     }):Play()
 end
 
-local _0x28=UDim2.new(0,75,0,75)
-local _0x29=UDim2.new(0,68,0,68)
+local _0x25=UDim2.new(0,75,0,75)
+local _0x26=UDim2.new(0,68,0,68)
 
-local function _0x2A()
-    _0x20(_0x29,Color3.fromRGB(0,255,100),0,Color3.fromRGB(0,0,0),0.2,0)
+local function _0x27()
+    _0x1D(_0x26,Color3.fromRGB(0,255,100),0,Color3.fromRGB(0,0,0),0.2,0)
     
-    if not _0x10 then
-        _0x10=_0xB.RenderStepped:Connect(_0x16)
+    if not _0x7 then
+        _0x7=_0x2.RenderStepped:Connect(_0x13)
     end
 end
 
-local function _0x2B()
-    if _0x11 then
-        _0x11:Disconnect()
-        _0x11=nil
+local function _0x28()
+    if _0x8 then
+        _0x8:Disconnect()
+        _0x8=nil
     end
     
-    _0x20(_0x28,Color3.fromRGB(255,255,255),0.7,Color3.fromRGB(0,0,0),0.9,0.6)
+    _0x1D(_0x25,Color3.fromRGB(255,255,255),0.7,Color3.fromRGB(0,0,0),0.9,0.6)
     
-    if _0x10 then
-        _0x10:Disconnect()
-        _0x10=nil
+    if _0x7 then
+        _0x7:Disconnect()
+        _0x7=nil
     end
 end
 
-_0x13.InputBegan:Connect(function(_0x2C)
-    if _0x2C.UserInputType==Enum.UserInputType.MouseButton1 or _0x2C.UserInputType==Enum.UserInputType.Touch then
-        _0x2A()
+_0x10.InputBegan:Connect(function(_0x29)
+    if _0x29.UserInputType==Enum.UserInputType.MouseButton1 or _0x29.UserInputType==Enum.UserInputType.Touch then
+        _0x27()
         
-        _0x11=_0x2C.Changed:Connect(function()
-            if _0x2C.UserInputState==Enum.UserInputState.End then
-                _0x2B()
+        _0x8=_0x29.Changed:Connect(function()
+            if _0x29.UserInputState==Enum.UserInputState.End then
+                _0x28()
             end
         end)
     end
